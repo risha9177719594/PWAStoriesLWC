117384710d05601964efc9440af3edf5a12b474c
rajendar bommidi <risha.9177719594@gmail.com>
2020-08-09 16:37:05 +0530

    Add files via upload

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -1,23 +1,16 @@
 import { LightningElement } from 'lwc';
 
 const CONTACTS_URL = 'api/contacts';
-const STORIES_URL = 'api/stories';
 
 export default class App extends LightningElement {
 
     contacts = [];
-    stories = [];
 
     connectedCallback() {
         fetch(CONTACTS_URL).then(response => {
             return response.json();
         }).then( contacts => {
             this.contacts = contacts;
-        });
-        fetch(STORIES_URL).then(response => {
-            return response.json();
-        }).then( stories => {
-            this.stories = stories;
         });
     }
 
