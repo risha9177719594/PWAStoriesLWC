eacbfac238d4917a5f8c455ab8461db49306abd8
risha.9177719594@gmail.com <risha.9177719594@gmail.com>
2020-08-15 16:14:52 +0000

    changes for new modue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { createElement } from 'lwc';
 import MyApp from 'my/app';
+import MyStory from 'my/stories';
 
 const app = createElement('my-app', { is: MyApp });
+const str = createElement('my-stories', { is: MyStory });
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
+document.querySelector('#main').appendChild(str);
