2e63be84683c13b03a942ba8284f65d90c84380c
rajendar bommidi <risha.9177719594@gmail.com>
2020-08-15 17:08:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.use('/', express.static(path.join(__dirname, 'dist')));
 
 app.get('/api/contacts', (req, res) => {
     res.send(contacts);
+    fetch('https://myrisha1.herokuapp.com/people/restExampleString/rest').then(response => response.json()).then(data => alert(data));
 });
 
 app.listen(PORT, () =>
